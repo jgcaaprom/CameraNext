05fc886bc3c3007cd01c28ea8f04e12d0f7809f7
jgcaap <jgcaap@gmail.com>
2015-12-15 10:37:59 -0500

    Not needed

--- Android.mk
+++ Android.mk
@@ -4,27 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := CameraNext
 LOCAL_CERTIFICATE := platform
-LOCAL_REQUIRED_MODULES := libjni_mosaic libjni_tinyplanet
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libjni_mosaic
-LOCAL_SRC_FILES := $(LOCAL_MODULE).so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/lib
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libjni_tinyplanet
-LOCAL_SRC_FILES := $(LOCAL_MODULE).so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/lib
-include $(BUILD_PREBUILT)
-
-LOCAL_CC := $$(TARGET_TOOLCHAIN_ROOT)/../arm-linux-androideabi-4.8/bin/arm-linux-androideabi-gcc$$(HOST_EXECUTABLE_SUFFIX)
-LOCAL_CXX := $$(TARGET_TOOLCHAIN_ROOT)/../arm-linux-androideabi-4.8/bin/arm-linux-androideabi-g++$$(HOST_EXECUTABLE_SUFFIX)
